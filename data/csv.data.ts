373bc82fecbf54dcb05ad8f4c1f1221b02c4a3c6
Oliver Butterley <oliver.butterley@gmail.com>
2023-10-08 14:52:00 +0200

    🎨

--- data/csv.data.ts
+++ data/csv.data.ts
@@ -1,19 +1,13 @@
 // diary.data.js
+//
+// all csv files will be converted to json and made available as the data object
+// import { data } from './data/csv.data.ts'
+// this object contains a separate property for each imported csv file
+// i.e., if the file is diary.csv then data.diary will be the json version of the file
 
 import fs from "node:fs";
 import path from "node:path";
 import { parse } from "csv-parse/sync";
-
-// export default {
-//   watch: ["./diary.csv"],
-//   load(watchedFiles: string[]) {
-//     const file = watchedFiles[0];
-//     return parse(fs.readFileSync(file, "utf-8"), {
-//       columns: true,
-//       skip_empty_lines: true,
-//     });
-//   },
-// };
 
 export default {
   watch: ["./*.csv"],
